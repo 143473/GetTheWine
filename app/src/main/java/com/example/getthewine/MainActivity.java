99db116c9144543ce61e32055dc11981aeea3a4d
Claudiu <em_dudu@yahoo.com>
2022-04-26 20:02:15 +0200

    implemented camerax

--- app/src/main/java/com/example/getthewine/MainActivity.java
+++ app/src/main/java/com/example/getthewine/MainActivity.java
@@ -1,19 +1,51 @@
 package com.example.getthewine;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.ContentValues;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.common.util.concurrent.ListenableFuture;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity{
 
     private TabLayout tabs;
     private ViewPager2 viewPager2;
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView;
+    private ImageCapture imageCapture;
+
+    Button takePicture, savePicture;
+    private ImageAnalysis imageAnalysis;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +64,98 @@ public class MainActivity extends AppCompatActivity {
                 tab.setText("Tab " + (position+1));
             }
         }).attach();
+
+//        //Camera stuff + buttons
+//
+//        previewView = findViewById(R.id.previewView);
+//
+//        takePicture = findViewById(R.id.takePicture);
+//        savePicture = findViewById(R.id.savePicture);
+//
+//        takePicture.setOnClickListener(this);
+//        savePicture.setOnClickListener(this);
+//
+//        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+//        cameraProviderFuture.addListener(() ->{
+//            try{
+//                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+//                startCameraX(cameraProvider);
+//            }
+//            catch (ExecutionException | InterruptedException e){
+//                e.printStackTrace();
+//            }
+//        }, getExecutor());
     }
+//
+//    private void startCameraX(ProcessCameraProvider cameraProvider) {
+//        cameraProvider.unbindAll();
+//
+//        //Camera selector use case
+//        CameraSelector cameraSelector = new CameraSelector.Builder()
+//                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+//                .build();
+//
+//        //Preview use case
+//        Preview preview = new Preview.Builder().build();
+//
+//        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+//
+//        //Image capture use case
+//        imageCapture = new ImageCapture.Builder()
+//                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+//                .build();
+//
+//        //Image analysis use case
+//        imageAnalysis = new ImageAnalysis.Builder()
+//                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+//                .build();
+//
+//        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalysis);
+//    }
+//
+//
+//    private Executor getExecutor() {
+//        return ContextCompat.getMainExecutor(this);
+//    }
+//
+//    @Override
+//    public void onClick(View view) {
+//        capturePhoto();
+//    }
+//
+//    private void capturePhoto() {
+//        long timestamp = System.currentTimeMillis();
+//
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
+//        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+//
+//
+//        imageCapture.takePicture(
+//                new ImageCapture.OutputFileOptions.Builder(
+//                        getContentResolver(),
+//                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+//                        contentValues
+//                ).build(),
+//                getExecutor(),
+//                new ImageCapture.OnImageSavedCallback() {
+//                    @Override
+//                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+//                        Toast.makeText(MainActivity.this, "Photo saved successfully.", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    @Override
+//                    public void onError(@NonNull ImageCaptureException exception) {
+//                        Toast.makeText(MainActivity.this, "Error saving the photo: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//        );
+//    }
+//
+//    @Override
+//    public void analyze(@NonNull ImageProxy image) {
+//        // Image processing here for the current frame
+//        Log.d(TAG,"analyze the frame at: " + image.getImageInfo().getTimestamp());
+//        image.close();
+//    }
 }
--- app/src/main/java/com/example/getthewine/page1.java
+++ app/src/main/java/com/example/getthewine/page1.java
@@ -1,64 +1,163 @@
 package com.example.getthewine;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link page1#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class page1 extends Fragment {
+import com.google.common.util.concurrent.ListenableFuture;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.io.File;
+import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class page1 extends Fragment implements View.OnClickListener, ImageAnalysis.Analyzer{
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView;
+    private ImageCapture imageCapture;
+
+    Button takePicture, savePicture;
+    private ImageAnalysis imageAnalysis;
+
+    private Context context = null;
+    private View rootView = null;
 
     public page1() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment page1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static page1 newInstance(String param1, String param2) {
-        page1 fragment = new page1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page1, container, false);
+        rootView = inflater.inflate(R.layout.fragment_page1, container, false);
+        context = this.getActivity();
+
+        //Camera stuff + buttons
+
+        previewView = rootView.findViewById(R.id.previewView);
+
+        takePicture = rootView.findViewById(R.id.takePicture);
+        savePicture = rootView.findViewById(R.id.savePicture);
+
+        takePicture.setOnClickListener(this);
+        savePicture.setOnClickListener(this);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(context);
+        cameraProviderFuture.addListener(() ->{
+            try{
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            }
+            catch (ExecutionException | InterruptedException e){
+                e.printStackTrace();
+            }
+        }, getExecutor());
+        return  rootView;
+    }
+
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+
+        //Camera selector use case
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        //Preview use case
+        Preview preview = new Preview.Builder().build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        //Image capture use case
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+
+        //Image analysis use case
+        imageAnalysis = new ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalysis);
+    }
+
+
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(context);
+    }
+
+    @Override
+    public void onClick(View view) {
+        capturePhoto();
+    }
+
+    private void capturePhoto() {
+        long timestamp = System.currentTimeMillis();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+        imageCapture.takePicture(
+                new ImageCapture.OutputFileOptions.Builder(
+                        context.getContentResolver(),
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues
+                ).build(),
+                getExecutor(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        Toast.makeText(context, "Photo saved successfully.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(context, "Error saving the photo: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void analyze(@NonNull ImageProxy image) {
+        // Image processing here for the current frame
+        Log.d(TAG, "analyze the frame at: " + image.getImageInfo().getTimestamp());
+        image.close();
     }
 }
